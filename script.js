41c1e2dbb73e10e787b5e7a74855ceb6996b8d2b
prathamesh <prathamesh.math@gmail.com>
2024-10-20 18:18:53 +0530

    Add fibs recursive

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const fibs = require('./fibs.js')
+const fibsRec = require('./fibsRec.js')
 
 console.log(fibs(8));
+console.log(fibs(0));
+console.log(fibs(1));
+console.log(fibs(2));
+console.log(fibs(3));
+console.log(fibs(4));
+console.log(fibs(5));
+
+console.log('\nRecursive\n');
+
+console.log(fibsRec(8));
+console.log(fibs(0));
+console.log(fibs(1));
+console.log(fibs(2));
+console.log(fibs(3));
+console.log(fibs(4));
+console.log(fibs(5));
